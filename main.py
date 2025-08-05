4ddf0fe40112c8f1ca49618d0b1a0896bf5bf24c
Ash <dev.achraf.hassine@gmail.com>
2025-08-05 21:04:27 +0200

    extract max_workers as constant

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import typer
 import inquirer
 from datetime import datetime
 
+# Max number of threads to use for downloading
+MAX_WORKERS = 5
+
 dlinks_queue = queue.Queue()  # Thread-safe queue for storing download links
 HEADERS = ({
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
